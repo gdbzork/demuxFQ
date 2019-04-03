2a556384a97217caf8d135f977799bdb11a724d1
gdbzork <gdbzork@gmail.com>
2019-04-03 14:15:15 +0100

    added some test cases

--- test/sampleSheet_test.cpp
+++ test/sampleSheet_test.cpp
@@ -1,13 +1,9 @@
-#define CATCH_CONFIG_MAIN
-
 #include <string.h>
 #include "catch.hpp"
 #include "config.h"
 #include "sampleSheet.h"
 
 TEST_CASE("Sample Sheets","[SampleSheet]") {
-//  std::string fn = sprintf("%s/%s",(char *)demux_BINARY_DIR,(char *)"test/testdata/sampleSheet_regular.txt");
-  // std::string fnS (demux_BINARY_DIR), ("test/testdata/sampleSheet_regular.txt");
   char fn[100] = demux_BINARY_DIR;
   strcat(fn,"/test/testdata/sampleSheet_regular.txt");
   fprintf(stderr,"'%s'\n",fn);
@@ -17,35 +13,35 @@ TEST_CASE("Sample Sheets","[SampleSheet]") {
   REQUIRE(s != nullptr);
 }
 
-/*
-    void testSingle(void) {
-      char fn[100] = "@srcdir@/testdata/sampleSheet_oneLine.txt";
-      SampleSheet *s;
-      Index::clearStaticData();
-      s = SampleSheet::load(fn,true,false);
-      TS_ASSERT(s != NULL);
-    }
+TEST_CASE("Single Index","[SampleSheet]") {
+  char fn[100] = demux_BINARY_DIR;
+  strcat(fn,"/test/testdata/sampleSheet_oneLine.txt");
+  SampleSheet *s;
+  Index::clearStaticData();
+  s = SampleSheet::load(fn,true,false);
+  REQUIRE(s != nullptr);
+}
 
-    void testNoEoln(void) {
-      char fn[100] = "@srcdir@/testdata/sampleSheet_noEoln.txt";
-      SampleSheet *s;
-      Index::clearStaticData();
-      s = SampleSheet::load(fn,true,false);
-      TS_ASSERT(s == NULL);
-    }
+TEST_CASE("No Eoln","[SampleSheet]") {
+  char fn[100] = demux_BINARY_DIR;
+  strcat(fn,"/test/testdata/sampleSheet_noEoln.txt");
+  SampleSheet *s;
+  Index::clearStaticData();
+  s = SampleSheet::load(fn,true,false);
+  REQUIRE(s == nullptr);
+}
 
-    void testReverse(void) {
-      char fn[100] = "@srcdir@/testdata/sampleSheet_regular.txt";
-      SampleSheet *s;
-      Index::clearStaticData();
-      s = SampleSheet::load(fn,true,true);
-      TS_ASSERT(s != NULL);
-      Index* asrc = s->getIndex(0);
-      Index* bsrc = s->getIndex(1);
-      Index* atest = new Index("TAAGGCGA","GCGATCTA");
-      Index* btest = new Index("TAAGGCGA","GCGGCCTT");
-      TS_ASSERT(asrc->isMatch(*atest,0));
-      TS_ASSERT(bsrc->isMatch(*btest,0));
-    }
-};
-*/
+TEST_CASE("Reverse","[SampleSheet]") {
+  char fn[100] = demux_BINARY_DIR;
+  strcat(fn,"/test/testdata/sampleSheet_regular.txt");
+  SampleSheet *s;
+  Index::clearStaticData();
+  s = SampleSheet::load(fn,true,true);
+  REQUIRE(s != nullptr);
+  Index* asrc = s->getIndex(0);
+  Index* bsrc = s->getIndex(1);
+  Index* atest = new Index("TAAGGCGA","GCGATCTA");
+  Index* btest = new Index("TAAGGCGA","GCGGCCTT");
+  REQUIRE(asrc->isMatch(*atest,0));
+  REQUIRE(bsrc->isMatch(*btest,0));
+}
